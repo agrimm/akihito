0e1efda06b940d0b675a45884bc379bfb77537eb
Andrew Grimm <andrew.j.grimm@gmail.com>
2015-04-24 23:42:44 +1000

    Fix broken spec fixtures

--- spec/factories/pmc_article_list_factory.rb
+++ spec/factories/pmc_article_list_factory.rb
@@ -2,7 +2,7 @@ require 'pmc_article_list'
 
 FactoryGirl.define do
   factory :pmc_article_list, class: PMCArticleList do
-    filenames ['../data_akihito/pubmed_central/Gene/Gene_2008_Feb_29_410(1-2)_165-176.nxml']
+    filenames ['../data_akihito/Gene/Gene_2008_Feb_29_410(1-2)_165-176.nxml']
     initialize_with { new(filenames) }
   end
 end
